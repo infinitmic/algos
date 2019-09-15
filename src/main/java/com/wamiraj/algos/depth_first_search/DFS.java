b2ee4ff5a4ba37164e18bd37c9ba611e45ede46b
Prabhat Ranjan <prabhat.raj18@gmail.com>
2019-09-15 16:29:12 +0530

    dfs implementation

---
+++ src/main/java/com/wamiraj/algos/depth_first_search/DFS.java
@@ -0,0 +1,6 @@
+package com.wamiraj.algos.depth_first_search;
+
+public class DFS {
+
+
+}
---
+++ src/main/java/com/wamiraj/algos/depth_first_search/Vertex.java
@@ -0,0 +1,46 @@
+package com.wamiraj.algos.depth_first_search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+
+    private int data;
+    private boolean visited;
+    private List<Vertex> neighbourVertextList;
+
+    public Vertex(int data){
+        this.data = data;
+        visited = false;
+        neighbourVertextList = new ArrayList<>();
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public void addNeighbour(Vertex vertex) {
+        neighbourVertextList.add(vertex);
+    }
+
+    public List<Vertex> getNeighbourVertextList() {
+        return neighbourVertextList;
+    }
+
+    @Override
+    public String toString() {
+        return " " + data;
+    }
+}
